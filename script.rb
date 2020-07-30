905e14e602308e0748da15120636ec9c9393f2ae
Sébastien Gaya <sebastien.gaya@gmail.com>
2020-07-30 10:00:03 +0200

    escape

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
 puts "PPTX to Medias by SebouChu"
 puts "========================="
 
-input_file = ARGV[0]
-file_type = `file -b #{input_file}`
-file_ext = File.extname(input_file)
+file_path = ARGV[0]
+file_type = `file -b "#{file_path}"`
+file_ext = File.extname(file_path)
 
-puts "- Input file: #{input_file}"
+puts "- Input file: #{file_path}"
 puts "- File type: #{file_type}"
 
 unless file_type.include?('PowerPoint') && file_ext == '.pptx'
@@ -15,19 +15,19 @@ end
 
 puts 'Input file is a PowerPoint presentation. Processing...'
 
-dir = File.dirname(input_file)
-basename = File.basename(input_file, file_ext)
+dir = File.dirname(file_path)
+basename = File.basename(file_path, file_ext)
 
-system("cp #{input_file} #{dir}/archive.zip")
-system("unzip #{dir}/archive.zip ppt/media/* -d #{dir}")
-system("mv #{dir}/ppt/media #{dir}/#{basename}_media")
+system("cp \"#{file_path}\" \"#{dir}/archive.zip\"")
+system("unzip \"#{dir}/archive.zip\" ppt/media/* -d #{dir}")
+system("mv \"#{dir}/ppt/media\" \"#{dir}/#{basename}_media\"")
 
-medias_count = `ls -1 #{dir}/#{basename}_media | wc -l`.strip.to_i
-videos_count = `ls -1 #{dir}/#{basename}_media | grep .mp4 | wc -l`.strip.to_i
+medias_count = `ls -1 \"#{dir}/#{basename}_media\" | wc -l`.strip.to_i
+videos_count = `ls -1 \"#{dir}/#{basename}_media\" | grep .mp4 | wc -l`.strip.to_i
 
 puts "File has been processed. Found #{medias_count} medias (including #{videos_count} videos). Cleaning..."
 
-system("rmdir #{dir}/ppt")
-system("rm #{dir}/archive.zip")
+system("rmdir \"#{dir}/ppt\"")
+system("rm \"#{dir}/archive.zip\"")
 
 puts "Done!"
